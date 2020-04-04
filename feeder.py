2f5dde14e9490cc3888f1c158e8b5a86b8d799e8
Sauraj <saureyj43@gmail.com>
2020-04-04 19:10:48 +0530

    Added "select_feed()" method for Parser. Adjusted select_feed parameter for scraping abstract according to the publisher type Added "publishers" class variable Added "headers" class variable for user agent scripting

--- feeder.py
+++ feeder.py
@@ -1,14 +1,17 @@
 from feedparser import parse
-import warnings
+from collections import namedtuple
+import requests
+from bs4 import BeautifulSoup
 
 """
 Variables to extract:
 
-Title of the paper 
-Publication date 
+Title of the paper (Done)
+Publication date  (Done)
 Journal Volume 
 Authors 
-Direct link to the paper 
+Direct link to the paper (Done)
+Abstract 
 """
 
 class Parser:
@@ -20,19 +23,22 @@ class Parser:
 		'KBS': "http://rss.sciencedirect.com/publication/science/09507051",
 		'NN': "http://rss.sciencedirect.com/publication/science/08936080",
 		'NC': "http://rss.sciencedirect.com/publication/science/09252312",
-		'PR': "http://rss.sciencedirect.com/publication/science/00313203",
 		'JCF': "http://rss.sciencedirect.com/publication/science/09291199",
 		'JOF': "https://onlinelibrary.wiley.com/feed/15406261/most-recent",
-		'JOFE': "https://academic.oup.com/rss/site_5193/3058.xml",
-		'AAP': "https://projecteuclid.org/feeds/euclid.aoap_article_rss.xml"
+		'JOFE': "https://academic.oup.com/rss/site_5193/3058.xml"
 	}
+	publishers = {
+		'Elsevier': ['ESWA','IJF','BDR','DSS','KBS','NN','NC','PR','JCF'],
+		'Wiley': ['JOF', 'JOFE']
+	}
+	#User agent for crawling the webpage without giving error 404/403
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
 
 
-	def __init__(self, feeds, journal_name=str,show_abstract=bool):
+	def __init__(self, feeds, journal_name=str):
 		self.journal_name = journal_name
 		self.feeds = feeds
-		self.show_abstract = show_abstract
-
 		self.feeder = parse(self.journals[self.journal_name])
 		self.journal_info = self.feeder['feed']['title']
 
@@ -41,23 +47,47 @@ class Parser:
 
 	def fetch_feeds(self):
 		total_count = len(self.feeder.entries) #Maximum number of feeds available on the RSS
-		if (self.feeds == 'all'):
+		if self.feeds == 'all':
 			self.feeds = total_count
 
 		if type(self.feeds) == int:
+			try:
+				assert self.feeds < total_count
+			except:
+				self.feeds = total_count
+
 			print("Showing Recent {} feeds".format(self.feeds))
 			for i in range(self.feeds):
 				title = self.feeder.entries[i].title
 				print("{}. {}, ".format(i+1, title))
 
 	def select_feed(self, feed_number):
-		url =
+		for key in self.publishers.keys():
+			if self.journal_name in self.publishers['Elsevier']:
+				url = self.feeder.entries[feed_number-1].id
+
+				#Generate request for the ScienceDirect URL link
+				page = requests.get(url, headers=self.headers)
+				paper_page = BeautifulSoup(page.content, 'html.parser')
+				abstr = paper_page.find(id="sp0002").string #Find a bypass for 'string' method
+
+
+			if self.journal_name in self.publishers['Wiley']:
+				abstr = BeautifulSoup(self.feeder.entries[feed_number-1].summary,
+				                      'html.parser').get_text()
+
+
+		paper_info = namedtuple('PaperInfo', ['title','url','abstract'])
+		paper = paper_info(self.feeder.entries[feed_number-1].title,
+		                   self.feeder.entries[feed_number-1].id,
+		                   abstr)
+		print(paper.title)
+		print(paper.url)
+		print(paper.abstract)
 
 
 
-
-
-
-es = Parser(22, 'ESWA',  show_abstract=True)
+es = Parser(100, 'ESWA')
 es.fetch_feeds()
+es.select_feed(2)
 
--- tester.py
+++ tester.py
@@ -1,10 +1,24 @@
 from feedparser import parse
 from bs4 import BeautifulSoup
 
-data = parse("http://rss.sciencedirect.com/publication/science/09574174")
-print(len(data.entries))
-print(data.entries[0].id)
-print(data.entries[0].summary)
+data = parse("https://onlinelibrary.wiley.com/feed/15406261/most-recent")
+# print(data)
+
+# print(len(data.entries))
+# print(data.entries[0].title)
+# print(data.entries[0].id)
+# print(data.entries[0].summary)
 
 soup = BeautifulSoup(data.entries[0].summary, 'html.parser')
-print(soup.prettify())
+print(soup.get_text())
+
+# publishers = {
+# 	'Elsevier': ['ESWA', 'IJF', 'BDR', 'DSS', 'KBS', 'NN', 'NC', 'PR', 'JCF'],
+# 	'Wiley': ['JOF', 'JOFE']
+# }
+#
+# for key in publishers.keys():
+# 	if 'ESWA' in publishers[key]:
+# 		print(key)
+# 		break
+#
